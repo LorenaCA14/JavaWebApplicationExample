16988bdce2ce58e6598c4a3c1b5f78544d842b30
LC <lc_plus14@hotmail.com>
2024-09-12 18:34:06 +0200

    Actividad 1. Servlets - Optativa

--- src/main/java/ejemplos/servlet/curso/MyServlet2.java
+++ src/main/java/ejemplos/servlet/curso/MyServlet2.java
@@ -8,6 +8,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+
 @WebServlet(urlPatterns = "/myServlet2")
 public class MyServlet2 extends HttpServlet {
 
